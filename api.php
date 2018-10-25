45533bfc45ae8928aa714ab024e221a7b9ff32d1
HiepTV <35910113+ffse1701003@users.noreply.github.com>
2018-10-25 11:36:33 +0700

    wordpress plugin designit

---
+++ api.php
@@ -0,0 +1,97 @@
+<?php
+/**
+* Grab latest post title by an author!
+*
+* @param array $data Options for the function.
+* @return string|null Post title for the latest,  * or null if none.
+*/
+
+add_filter('media_upload_tabs', function($tabs) {
+    $tabs['dbsdk_grabfromurl'] = __('Designit');
+    return($tabs);
+});
+
+add_action('media_upload_dbsdk_grabfromurl', function() {
+    return dbsdk_HandleUpload();
+});
+
+// Check image name extension
+function dbsdk_fileType($fileType = NULL){
+    $result = '';
+    $arr = array(
+        'png' => 'image/png',
+        'jpg' => 'image/jpeg',
+        'gif' => 'image/gif',
+        'bmp' => 'image/bmp',
+        'tif' => 'image/tiff',
+        'svg' => 'image/svg+xml',
+    );
+
+    foreach ($arr as $key => $value) {
+        if($value == $fileType){
+            $result = $key;
+        }
+    }
+
+    return $result;
+}
+
+function dbsdk_HandleUpload() {
+    $flag = true;
+    $post_id = $_REQUEST['post_id'] ? $_REQUEST['post_id'] : 0;
+    $dbsdk_grabfrom_url = $_POST['dbsdk_grabfrom_url'];
+    $dbsdk_file_name = $_POST['dbsdk_file_name'];
+
+    if ( isset( $dbsdk_grabfrom_url ) && $dbsdk_grabfrom_url != '') {
+
+        $file_array = array();
+        $file_array['tmp_name'] = download_url($dbsdk_grabfrom_url);
+
+        // Get info image
+        $fileType = getimagesize ($file_array['tmp_name']);
+        $image_type = $fileType[2];
+     
+        // Check file_array is an image or not
+        if(!in_array($image_type , array(IMAGETYPE_GIF , IMAGETYPE_JPEG ,IMAGETYPE_PNG , IMAGETYPE_BMP))){
+            $flag = false;
+        }
+
+        // Check image name extension
+        $ex = dbsdk_fileType($fileType['mime']);
+
+        if($ex != '' && $flag == true){
+            $file_array['name'] = $dbsdk_file_name . '.' . $ex;
+
+            if (is_wp_error($file_array['tmp_name'])) {
+                @unlink($file_array['tmp_name']);
+                return new WP_Error('grabfromurl', 'Could not download image from remote source');
+            }
+
+            $attachmentId = media_handle_sideload($file_array, $post_id);
+
+            $obj_data = (object)[];
+
+            if( $attachmentId ){
+                // create the thumbnails
+                $attach_data = wp_generate_attachment_metadata($attachmentId, get_attached_file($attachmentId));
+
+                wp_update_attachment_metadata( $attachmentId,  $attach_data );
+                
+                // Get image info in media library after upload image on wordpress
+                $arr_info_image = wp_get_attachment_image_src($attachmentId, array('700', '600'), "", array( "class" => "img-responsive" ));
+
+                $arr_temp = array('url' => $arr_info_image[0], 'width' => $arr_info_image[1], 'height' => $arr_info_image[2], 'is_intermediate' => $arr_info_image[3]);
+                
+                $obj_data->image_info = $arr_temp;
+                $obj_data->post_id = $post_id;
+            }
+            
+            header("Content-type: application/json; charset=utf-8");
+            echo json_encode($obj_data);
+        }
+    }else{
+        $errors = array();
+        $id = 0;
+        return wp_iframe( 'media_upload_type_form', 'image', $errors, $id );
+    }
+}
---
+++ designit.php
@@ -0,0 +1,53 @@
+<?php
+/*
+Plugin Name: Designit
+Plugin URI: https://www.designbold.com/collection/create-new
+Description: Desingbold designit build plugin allow designning image online
+Version: 1.1.0
+Author: Designit
+Author URI: https://www.designbold.com/
+License: GPLv2 or later
+*/
+
+/*
+{Designit} is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+ 
+{Designit} is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+ 
+You should have received a copy of the GNU General Public License
+along with {Designit}. If not, see {Plugin URI}.
+*/
+
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+
+if(!class_exists('Designit')) {
+	class Designit {
+		function __construct() {
+			global $post;
+			include_once('api.php');
+			wp_enqueue_style( 'style', plugin_dir_url(__FILE__) . '/css/main.css', false, '1.1', 'all' );
+			wp_enqueue_script( 'designTool', plugin_dir_url(__FILE__) . '/button.js', array ( 'jquery' ), 1.1, true);
+			wp_localize_script('designTool', 'WPURLS', array( 'siteurl' => get_option('siteurl') )); 
+			add_action( 'media_buttons', array($this, 'dbsdk_createButton'));
+		}
+
+		function dbsdk_createButton(){
+			global $post;
+			$output = '';
+			$icon = plugin_dir_url(__FILE__) . 'logo_b.svg';
+
+			$img = '<span class="wp-media-buttons-icon" style="background-image: url(' . $icon . '); width: 16px; height: 16px; margin-top: 1px;"></span>';
+
+			$output = '<button type="button" class="button maxbutton_media_button" onclick="DBSDK.startOverlay('. $post->ID .')" title="Image design" style="padding-left: .4em;">' . $img . ' Image design</button>';
+
+			echo $output;
+		}
+	}
+}
+$dbii = new Designit();
